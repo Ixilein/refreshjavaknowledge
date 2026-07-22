de22109e64b306f0a374ecce1c13c9c56b6f65f9
Felix Stadler <120939125+Ixilein@users.noreply.github.com>
2026-07-22 08:48:10 +0200

    Made FizzBuzz (Prints 1 - 100; mod 3 = 0 with Fizz and mod 5 = 0 with Buzz

---
+++ src/main/java/de/felix_stadler/refreshjava/fizzbuzz/fizzbuzz.java
@@ -0,0 +1,19 @@
+package de.felix_stadler.refreshjava.fizzbuzz;
+
+public class fizzbuzz {
+    static void main() {
+        for (int i = 0; i <= 100;  i++){
+            System.out.print("\n" + i);
+            if(i % 3 == 0){
+                System.out.print(" Fizz");
+            }
+            else{
+                System.out.print(" ");
+            }
+            if (i % 5 == 0) {
+                System.out.print("Buzz");
+            }
+
+        }
+    }
+}
