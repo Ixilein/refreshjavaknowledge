74780385f334f02a8c2665539c2687743bd47d34
Felix Stadler <120939125+Ixilein@users.noreply.github.com>
2026-07-22 08:20:47 +0200

    made a working (awkward to use) two number calculator

--- src/main/java/de/felix_stadler/refreshjava/calculator/CalculatorRunner.java
+++ src/main/java/de/felix_stadler/refreshjava/calculator/CalculatorRunner.java
@@ -1,16 +1,18 @@
 package de.felix_stadler.refreshjava.calculator;
-import de.felix_stadler.refreshjava.calculator.Calculator;
 import java. util.Scanner;
 
 public class CalculatorRunner {
-    static void main(String[] args) {
+    static void main() {
         CalculatorRunner test = new CalculatorRunner();
+        Calculator calculator = new Calculator();
+        System.out.println(test.Operation(calculator, test.getNumber(), test.selectOperator(), test.getNumber()));
     }
 
     int selectOperator(){
         Scanner scanner = new Scanner(System.in);
         int selection;
-        do{ System.out.println("""
+        do{
+            System.out.println("""
                 Do you want to:
                 1. Add up the numbers
                 2. Subtract the first from the second number
@@ -18,7 +20,36 @@ public class CalculatorRunner {
                 4. Divide the second through the first number?""");
         selection = scanner.nextInt();}
         while (selection > 4 || selection < 1);
-
         return selection;
+    }
+
+    double getNumber(){
+        Scanner scanner = new Scanner(System.in);
+        double number;
+        System.out.println("Enter Number: ");
+        number = scanner.nextDouble();
+        return number;
+    }
+
+    double Operation(Calculator calc, double number1, int operator, double number2){
+        double res;
+        switch (operator) {
+            case 1:
+                res = calc.add(number1, number2);
+                break;
+            case 2:
+                res = calc.subtract(number1, number2);
+                break;
+            case 3:
+                res = calc.multiply(number1, number2);
+                break;
+            case 4:
+                res = calc.division(number1, number2);
+                break;
+            default:
+                res = 0;
+                break;
+        }
+        return res;
     }
 }
